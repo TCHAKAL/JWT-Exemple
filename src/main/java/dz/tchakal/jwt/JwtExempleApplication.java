0eeaff783df7c422362352cee225c051f703323b
TCHAKAL <ahmed.tchakal@gmail.com>
2022-09-04 08:42:58 +0100

    initial commit

---
+++ src/main/java/dz/tchakal/jwt/JwtExempleApplication.java
@@ -0,0 +1,13 @@
+package dz.tchakal.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtExempleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtExempleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dz/tchakal/jwt/JwtExempleApplicationTests.java
@@ -0,0 +1,13 @@
+package dz.tchakal.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtExempleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
